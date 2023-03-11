4e340c37ab66c778407eceee821a0764efdd065b
Andrii Kuz <andrykuz4556@gmail.com>
2023-03-12 00:51:07 +0200

    Solved conflicts

--- src/App.js
+++ src/App.js
@@ -1,10 +1,6 @@
 import Main from './components/Header';
 import FeedbackForm from './components/FeedbackForm';
-<<<<<<< HEAD
-import './App.css';
-=======
 
->>>>>>> deploy
 function App() {
   return (
     <div className="app">
